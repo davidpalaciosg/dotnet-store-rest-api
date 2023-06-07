19d9361beb314b3ea7db4c1a650dd9e4c6d841f8
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-06-07 10:29:44 -0500

    First commit, project started

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_products_rest_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
