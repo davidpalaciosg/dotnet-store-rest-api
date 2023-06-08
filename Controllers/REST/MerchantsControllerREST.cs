f39e63dff28daa954e9bc385a6a3d2e61a8c97b5
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-06-08 15:59:16 -0500

    Merchants Controller REST done

---
+++ Controllers/REST/MerchantsControllerREST.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnet_products_rest_api.Models;
+
+namespace dotnet_products_rest_api.Controllers.REST
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MerchantsControllerREST : ControllerBase
+	{
+		private readonly StoreDbContext _context;
+
+		public MerchantsControllerREST(StoreDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/MerchantsControllerREST
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Merchant>>> GetMerchants()
+		{
+			if (_context.Merchants == null)
+			{
+				return NotFound();
+			}
+			var completeMerchants = await _context.Merchants
+				.Include(m => m.CountryCodeNavigation)
+				.Include(m => m.Admin)
+				.Where(m => m.State == 1)
+				.ToListAsync();
+
+			return completeMerchants;
+		}
+
+		// GET: api/MerchantsControllerREST/5
+		[HttpGet("{id}")]
+		public ActionResult<Merchant> GetMerchant(uint id)
+		{
+			if (_context.Merchants == null)
+			{
+				return NotFound();
+			}
+			var merchant = GetMerchantByID(id).Result;
+
+			if (merchant == null || merchant.State == 0)
+			{
+				return NotFound();
+			}
+
+			return merchant;
+		}
+
+		// PUT: api/MerchantsControllerREST/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutMerchant(uint id, Merchant merchant)
+		{
+			if (id != merchant.Id)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(merchant).State = EntityState.Modified;
+
+			try
+			{
+				merchant.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+				merchant.State = 1;
+				_context.Merchants.Update(merchant);
+				await _context.SaveChangesAsync();
+				return Ok(merchant);
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!MerchantExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+		}
+
+		// POST: api/MerchantsControllerREST
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Merchant>> PostMerchant(Merchant merchant)
+		{
+			if (_context.Merchants == null)
+			{
+				return Problem("Entity set 'StoreDbContext.Merchants'  is null.");
+			}
+			merchant.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+
+			//Find CountryCode
+			var country = await _context.Countries.FindAsync(merchant.CountryCode);
+			if (country == null || country.State == 0)
+			{
+				return NotFound("CountryCode not found.");
+			}
+
+			//Find Admin
+			var admin = await _context.Users.FindAsync(merchant.AdminId);
+			if (admin == null || admin.State == 0)
+			{
+				return NotFound("Admin not found.");
+			}
+
+			//Add fields
+			merchant.CountryCodeNavigation = country;
+			merchant.Admin = admin;
+
+			//Save changes
+			_context.Merchants.Add(merchant);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetMerchant", new { id = merchant.Id }, merchant);
+		}
+
+		// DELETE: api/MerchantsControllerREST/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteMerchant(uint id)
+		{
+			if (_context.Merchants == null)
+			{
+				return NotFound();
+			}
+
+			var merchant = GetMerchantByID(id).Result;
+
+			if (merchant == null)
+			{
+				return NotFound();
+			}
+			//Update the state of the merchant
+			merchant.State = 0;
+			_context.Entry(merchant).State = EntityState.Modified;
+			_context.Merchants.Update(merchant);
+			await _context.SaveChangesAsync();
+
+			return Ok(merchant);
+		}
+
+		private bool MerchantExists(uint id)
+		{
+			return (_context.Merchants?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+
+		private async Task<Merchant?> GetMerchantByID(uint id)
+		{
+			return await _context.Merchants
+				.Include(m => m.CountryCodeNavigation)
+				.Include(m => m.Admin)
+				.FirstOrDefaultAsync(m => m.Id == id && m.State == 1);
+		}
+	}
+}
--- Controllers/REST/UsersControllerREST.cs
+++ Controllers/REST/UsersControllerREST.cs
@@ -9,136 +9,138 @@ using dotnet_products_rest_api.Models;
 
 namespace dotnet_products_rest_api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersControllerREST : ControllerBase
-    {
-        private readonly StoreDbContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersControllerREST : ControllerBase
+	{
+		private readonly StoreDbContext _context;
 
-        public UsersControllerREST(StoreDbContext context)
-        {
-            _context = context;
-        }
+		public UsersControllerREST(StoreDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
+		// GET: api/Users
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+		{
+			if (_context.Users == null)
+			{
+				return NotFound();
+			}
 
-            var completeUsers = await _context.Users
-                  .Include(u => u.CountryCodeNavigation)
-                  .Include(u => u.Merchants)
-                  .Include(u => u.Orders)
-                  .Where(u => u.State == 1)
-                  .ToListAsync();
+			var completeUsers = await _context.Users
+				  .Include(u => u.CountryCodeNavigation)
+				  .Where(u => u.State == 1)
+				  .ToListAsync();
 
-            return completeUsers;
-        }
+			return completeUsers;
+		}
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(uint id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
+		// GET: api/Users/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<User>> GetUser(uint id)
+		{
+			if (_context.Users == null)
+			{
+				return NotFound();
+			}
 
-            if (user == null || user.State == 0)
-            {
-                return NotFound();
-            }
+			var user = await _context.Users
+				.Include(u => u.CountryCodeNavigation)
+				.Where(u => u.Id == id && u.State == 1)
+				.FirstOrDefaultAsync();
 
-            return user;
-        }
+			if (user == null || user.State == 0)
+			{
+				return NotFound();
+			}
 
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(uint id, User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
+			return user;
+		}
 
-            _context.Entry(user).State = EntityState.Modified;
+		// PUT: api/Users/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutUser(uint id, User user)
+		{
+			if (id != user.Id)
+			{
+				return BadRequest();
+			}
 
-            try
-            {
-                user.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
-                _context.Users.Update(user);
-                await _context.SaveChangesAsync();
-                return Ok(user);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
+			_context.Entry(user).State = EntityState.Modified;
 
-        // POST: api/Users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'StoreDbContext.Users'  is null.");
-            }
-            
-            user.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
-            //Find and Add country
-            var c = await _context.Countries.FindAsync(user.CountryCode);
-            if (c == null)
-                return BadRequest("Country with code " + user.CountryCode + " not found.");
-                
+			try
+			{
+				user.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+				_context.Users.Update(user);
+				await _context.SaveChangesAsync();
+				return Ok(user);
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!UserExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+		}
 
-            user.CountryCodeNavigation = c;
+		// POST: api/Users
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<User>> PostUser(User user)
+		{
+			if (_context.Users == null)
+			{
+				return Problem("Entity set 'StoreDbContext.Users'  is null.");
+			}
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+			user.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+			//Find and Add country
+			var c = await _context.Countries.FindAsync(user.CountryCode);
+			if (c == null)
+				return BadRequest("Country with code " + user.CountryCode + " not found.");
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
-        }
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(uint id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            //Update the state of the user to 0
-            user.State = 0;
-            _context.Entry(user).State = EntityState.Modified;
-            _context.Users.Update(user);
-            await _context.SaveChangesAsync();
+			user.CountryCodeNavigation = c;
 
-            return Ok(user);
-        }
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
 
-        private bool UserExists(uint id)
-        {
-            return (_context.Users?.Any(e => e.Id == id && e.State == 1)).GetValueOrDefault();
-        }
-    }
+			return CreatedAtAction("GetUser", new { id = user.Id }, user);
+		}
+
+		// DELETE: api/Users/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteUser(uint id)
+		{
+			if (_context.Users == null)
+			{
+				return NotFound();
+			}
+			var user = await _context.Users.FindAsync(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			//Update the state of the user to 0
+			user.State = 0;
+			_context.Entry(user).State = EntityState.Modified;
+			_context.Users.Update(user);
+			await _context.SaveChangesAsync();
+
+			return Ok(user);
+		}
+
+		private bool UserExists(uint id)
+		{
+			return (_context.Users?.Any(e => e.Id == id && e.State == 1)).GetValueOrDefault();
+		}
+	}
 }
